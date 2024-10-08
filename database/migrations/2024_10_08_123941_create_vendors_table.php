c1418aced8715fb6dcf4b120bc666990370a1f23
alanprayogo <alanbp98@gmail.com>
2024-10-08 20:15:08 +0700

    add migration

--- database/migrations/2024_10_08_123941_create_vendors_table.php
+++ database/migrations/2024_10_08_123941_create_vendors_table.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('vendors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['Admin', 'Member'])->default('Member');
-            $table->rememberToken();
+            $table->unsignedBigInteger('product_id');
             $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('vendors');
     }
 };
